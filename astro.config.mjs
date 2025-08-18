8164c7df9a62f455c6b1431300d37d6f80fedf7d
Brent De Clercq <brent@dcbrent.com>
2025-08-18 12:47:09 +0200

    update readme

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://demo489.judocluboosterzele.be",
+  site: "https://demo489.judocluboosterzele.be", // TODO: change to base domain
 });
