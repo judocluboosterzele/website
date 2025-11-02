805b584dbc1bd29ee8961b1c2dc4b359ee85ef53
Brent De Clercq <brent@dcbrent.com>
2025-11-02 13:02:40 +0100

    verslagen: fix images

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,28 +2,37 @@ import { defineCollection, z } from "astro:content";
 
 const pages = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string().optional(),
-    type: z.enum(["info", "lesgevers", "verslagen"]).default("info"),
-    heroImage: z.string().optional(),
-    heroImageAlt: z.string().optional(),
-    order: z.number().optional(),
-    navOrder: z.array(z.string()).optional(),
-    date: z
-      .string()
-      .transform((val) => new Date(val))
-      .optional(),
-    lesgevers: z
-      .array(
-        z.object({
-          name: z.string(),
-          image: z.string(),
-          info: z.array(z.string()),
-        })
-      )
-      .optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string().optional(),
+      type: z.enum(["info", "lesgevers", "verslagen"]).default("info"),
+      heroImage: z.string().optional(),
+      heroImageAlt: z.string().optional(),
+      order: z.number().optional(),
+      navOrder: z.array(z.string()).optional(),
+      date: z
+        .string()
+        .transform((val) => new Date(val))
+        .optional(),
+      lesgevers: z
+        .array(
+          z.object({
+            name: z.string(),
+            image: z.string(),
+            info: z.array(z.string()),
+          })
+        )
+        .optional(),
+      images: z
+        .array(
+          z.object({
+            src: image(),
+            alt: z.string(),
+          })
+        )
+        .optional(),
+    }),
 });
 
 export const collections = {
